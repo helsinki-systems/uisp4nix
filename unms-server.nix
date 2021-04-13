59a2a678ad4cec481f7aeda5e4db1ea1d288517f
ajs124 <git@ajs124.de>
2021-04-13 18:12:20 +0200

    unms: 0.13.3 -> 1.3.10
    also adapt to nixpkgs 20.09

--- unms-server.nix
+++ unms-server.nix
@@ -1,92 +1,64 @@
-{ stdenv, stdenvNoCC, fetchurl, dockerTools, nodejs, nodePackages, python, yarn, yarn2nix-moretea, writeTextFile, pkgconfig, vips }:
+{ stdenv, stdenvNoCC, runCommandNoCC, fetchurl, dockerTools, nodejs, nodePackages, python3, yarn, yarn2nix-moretea, writeTextFile, pkgconfig, vips }:
 
 let
-  # nix run nixpkgs.skopeo -c skopeo --override-os linux --override-arch x86_64 inspect docker://docker.io/ubnt/unms:1.0.0-dev.15 | jq -r '.Digest'
-  version = "0.13.3";
-  tag = "0.13.3";
+  # nix run nixpkgs.skopeo -c skopeo --override-os linux --override-arch x86_64 inspect docker://docker.io/ubnt/unms:1.3.10 | jq -r '.Digest'
+  version = "1.3.10";
+  tag = version;
 
   unmsImage = dockerTools.pullImage {
     imageName = "ubnt/unms";
-    imageDigest = "sha256:37b6362f2a7b8d0b9907098211a8d9344d4a698fc651cf641c048a638882cb74";
-    sha256 = "1kh5zsll57115v2605dz06sx28bdkp16yrhww1f4wxffzq2qcph3";
+    imageDigest = "sha256:12fc462a722717d6b66dc52cba95ad15d29e5067aa5a8074817c515c16982a93";
+    sha256 = "0qdd5alxl76g9wd23qgy018y4m772siw2gy5rj7vjh186s79mlfv";
     finalImageTag = tag;
   };
 
-  unmsServerSrc = dockerTools.runWithOverlay {
-    name = "unms-app-${version}-src";
-    fromImage = unmsImage;
-    postMount = ''
-      mkdir -p $out
-      shopt -s extglob
-      cp -rL mnt/home/app/unms/!(node_modules) $out
-      shopt -u extglob
-    '';
-  };
-
-  startScript = writeTextFile {
-    name = "unms-start";
-    executable = true;
-    text = ''
-      #!${stdenv.shell}
-      export PATH="${stdenv.shellPackage}/bin:$PATH"
-      ${yarn}/bin/yarn start
-    '';
-  };
-
-  nodeHeaders = fetchurl {
-    url = "https://nodejs.org/download/release/v${nodejs.version}/node-v${nodejs.version}-headers.tar.gz";
-    sha256 = "0bjnkf6xmpzwzd02x8y56165flnigriazi455azvydi80xlyx5wy";
-  };
+  # FIXME: https://github.com/NixOS/nixpkgs/pull/80068
+  unmsServerSrc = runCommandNoCC "unms-server-src" {} ''
+    mkdir $out
+    tar xf ${dockerTools.runWithOverlay {
+      name = "unms-app-${version}-src";
+      diskSize = 2048;
+      fromImage = unmsImage;
+      postMount = ''
+        mkdir -p $out
+        set -x
+        tar cf $out/layer.tar -C mnt/home/app/unms --exclude=node_modules --exclude=.wh..opq .
+        echo ${version} > $out/VERSION
+        touch $out/json # wat
+        shopt -u extglob
+      '';
+    }}/layer.tar -C $out
+  '';
 
   unms-server = yarn2nix-moretea.mkYarnPackage rec {
     src = unmsServerSrc;
-    packageJSON = ./package.json;
-    yarnLock = ./yarn.lock;
+    packageJSON = "${src}/package.json";
+    yarnLock = "${src}/yarn.lock";
+    yarnNix = ./yarn.nix;
     yarnFlags = yarn2nix-moretea.defaultYarnFlags ++ [ "--production" ];
 
-    pkgConfig = {
-      bcrypt = {
-        buildInputs = [ python nodePackages.node-gyp nodePackages.node-pre-gyp ];
+    pkgConfig = let
+      nodeGypRebuildPkg = {
+        buildInputs = [ python3 nodePackages.node-gyp ];
         postInstall = ''
-          node-pre-gyp configure build --build-from-source --tarball="${nodeHeaders}"
+          node-gyp rebuild --nodedir="${nodejs}"
         '';
       };
-
-      dtrace-provider = {
-        buildInputs = [ python nodePackages.node-gyp ];
-        postInstall = ''
-          node-gyp rebuild --tarball="${nodeHeaders}"
-        '';
-      };
-
-      heapdump = {
-        buildInputs = [ python nodePackages.node-gyp ];
-        postInstall = ''
-          node-gyp rebuild --tarball="${nodeHeaders}"
-        '';
-      };
-
-      raw-socket = {
-        buildInputs = [ python nodePackages.node-gyp ];
-        postInstall = ''
-          node-gyp rebuild --tarball="${nodeHeaders}";
-        '';
-      };
-
+    in {
+      heapdump = nodeGypRebuildPkg;
+      raw-socket = nodeGypRebuildPkg;
       sharp = {
-        buildInputs = [ pkgconfig vips python nodePackages.node-gyp ] ++ vips.buildInputs;
+        bcrypt = nodeGypRebuildPkg // {
+          buildInputs = nodeGypRebuildPkg.buildInputs ++ [ nodePackages.node-pre-gyp ];
+        };
+        buildInputs = nodeGypRebuildPkg.buildInputs ++ [ pkgconfig vips ] ++ vips.buildInputs;
         postInstall = ''
-          node-gyp rebuild --tarball="${nodeHeaders}"
+          ${nodeGypRebuildPkg.postInstall}
           node install/dll-copy
         '';
       };
     };
-
-    postInstall = ''
-      ln -s ${startScript} $out/bin/unms
-    '';
   };
-
 in {
   inherit unmsServerSrc unms-server;
 }
