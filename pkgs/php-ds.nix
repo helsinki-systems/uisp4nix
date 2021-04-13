3284db2d17566fc0765b7b72c46532fc2a85db27
ajs124 <git@ajs124.de>
2021-04-14 01:26:43 +0200

    pkgs/php-ds: init at 1.3.0

---
+++ pkgs/php-ds.nix
@@ -0,0 +1,27 @@
+{ php, lib, fetchFromGitHub
+, pcre2 }:
+php.buildPecl rec {
+  pname = "ds";
+  version = "1.3.0";
+
+  src = fetchFromGitHub {
+    owner = "php-ds";
+    repo = "ext-ds";
+    rev = "v${version}";
+    sha256 = "08fqn5q0p0d6qny08xkr2nnlkgmrr2chvda21ch231xnwd12s52p";
+  };
+
+  buildInputs = [
+    pcre2
+  ];
+
+  internalDeps = with php.extensions; [
+    json
+  ];
+
+  meta = with lib; {
+    description = "An extension providing efficient data structures for PHP 7";
+    license = with licenses; [ mit ];
+    maintainers = with maintainers; [ ajs124 das_j ] ++ teams.php.members;
+  };
+}
